7c6ff8d4bb61449bc62cf249578aedb0150a74ff
AKRISHT <akrisht2805@gmail.com>
2022-07-10 10:50:04 +0530

    Complete DFS treversal of Graph

---
+++ Graph Notes/dfs.cpp
@@ -0,0 +1,24 @@
+//****************************DFS Treversal**************************************//
+
+void dfs(int src,vector<int> &vis,vector<int> adjList[],,vector<int> &ans){
+    ans.push_back(src);
+    vis[src]=true;
+    for(auto it: adjList[src]){
+        if(!vis[it]){
+            dfs(it,vis,adjList,ans);
+        }
+    }
+}
+
+vector<int> bfsOfGraph(int V, vector<int> adjList[]) {
+
+    vector<int> vis(V,0);
+    vector<int> ans;
+    for(int i=0;i<V;i++){       //For the graph disconnected component
+        if(!vis[i]){
+            dfs(i,vis,adjList,ans);
+        }
+    }
+    return ans;
+    	
+}
